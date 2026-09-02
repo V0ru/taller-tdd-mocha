abf42a0bcefeff1998a866a2911a7fdfec6b3cf1
V0ru <JoseLucasOrtiz@hotmail.com>
2026-09-02 07:44:31 -0500

    Regla 2 Green HACER QUE PASE :b

--- src/validarPedido.js
+++ src/validarPedido.js
@@ -1,9 +1,15 @@
 function validarPedido(pedido) {
   const errores = [];
-//Bueno ahora vamos a escribir el codigo minimo necesario hacer que pase como dice el profe 
+
   // Regla 1: El total debe ser mayor a 0
   if (pedido.total <= 0) {
     errores.push('El total debe ser mayor a 0');
+  }
+
+  // Regla 2: El método de pago debe ser uno de: 'tarjeta', 'PSE', 'contraentrega' Ahora esta la validacion minima ecesaria para hacer que pase como dice el profe 
+  const metodosValidos = ['tarjeta', 'PSE', 'contraentrega'];
+  if (!metodosValidos.includes(pedido.metodoPago)) {
+    errores.push('Método de pago no válido');
   }
 
   return {
