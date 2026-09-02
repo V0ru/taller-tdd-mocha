8ed51cfb154e7589cf1408994d156e5a429b13ff
V0ru <JoseLucasOrtiz@hotmail.com>
2026-09-02 07:36:44 -0500

    Green hacer que pase

--- src/validarPedido.js
+++ src/validarPedido.js
@@ -1,6 +1,15 @@
 function validarPedido(pedido) {
-  // Fase RED: función sin implementar todavía
-  return {};
+  const errores = [];
+//Bueno ahora vamos a escribir el codigo minimo necesario hacer que pase como dice el profe 
+  // Regla 1: El total debe ser mayor a 0
+  if (pedido.total <= 0) {
+    errores.push('El total debe ser mayor a 0');
+  }
+
+  return {
+    valido: errores.length === 0,
+    errores: errores
+  };
 }
 
 module.exports = validarPedido;
