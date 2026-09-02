c31b7e1ebbd25210774431e150fd85686892e06e
V0ru <JoseLucasOrtiz@hotmail.com>
2026-09-02 07:48:43 -0500

    Regla 3 Red

--- test/validarPedido.test.js
+++ test/validarPedido.test.js
@@ -5,34 +5,54 @@ describe('validarPedido', () => {
 
   // Regla 1: El total debe ser mayor a 0
   it('el total del pedido debe ser mayor a 0', () => {
-    
+    // Arrange
     const pedido = {
       items: ['cuaderno'],
       total: 0,
       metodoPago: 'tarjeta'
     };
 
+    // Act
     const resultado = validarPedido(pedido);
 
-    
+    // Assert
     expect(resultado.valido).to.be.false;
     expect(resultado.errores).to.include('El total debe ser mayor a 0');
   });
 
-  // Regla 2:Bueno en la regla 2  el metodo de pago debe ser uno 
+  // Regla 2: El método de pago debe ser uno de: 'tarjeta', 'PSE', 'contraentrega'
   it('el método de pago debe ser tarjeta, PSE o contraentrega', () => {
-    // Arrange (aca preparamos un pedido pues con un metodo invalido como efectivo)
+    // Arrange
     const pedido = {
       items: ['cuaderno'],
       total: 50000,
       metodoPago: 'efectivo'
     };
 
-
+    // Act
     const resultado = validarPedido(pedido);
 
+    // Assert
     expect(resultado.valido).to.be.false;
     expect(resultado.errores).to.include('Método de pago no válido');
   });
 
+  // Regla 3: Si es contraentrega y supera 500000, no es válido
+  it('no debe permitir contraentrega para montos mayores a 500000', () => {
+    // Arrange
+    const pedido = {
+      items: ['cuaderno'],
+      total: 600000,
+      metodoPago: 'contraentrega'
+    };
+
+    // Act
+    const resultado = validarPedido(pedido);
+
+    // Assert
+    expect(resultado.valido).to.be.false;
+    expect(resultado.errores).to.include('El pago contraentrega no admite pedidos mayores a $500.000');
+  });
+
 });
+//Y bueno como todavia no hemos agregado esta condicion en la logica el test debe fallar que es practicamente lo que hemos echo en las demas reglas 
