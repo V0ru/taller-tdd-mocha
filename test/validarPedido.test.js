c35577652ade97c3d85daecb2c54df6b795a6146
V0ru <JoseLucasOrtiz@hotmail.com>
2026-09-02 07:41:58 -0500

    Regla 2 metodo de pago valido RED

--- test/validarPedido.test.js
+++ test/validarPedido.test.js
@@ -5,19 +5,34 @@ describe('validarPedido', () => {
 
   // Regla 1: El total debe ser mayor a 0
   it('el total del pedido debe ser mayor a 0', () => {
-    // Arrange
+    
     const pedido = {
       items: ['cuaderno'],
       total: 0,
       metodoPago: 'tarjeta'
     };
 
-    // Act
     const resultado = validarPedido(pedido);
 
-    // Assert
+    
     expect(resultado.valido).to.be.false;
     expect(resultado.errores).to.include('El total debe ser mayor a 0');
+  });
+
+  // Regla 2:Bueno en la regla 2  el metodo de pago debe ser uno 
+  it('el método de pago debe ser tarjeta, PSE o contraentrega', () => {
+    // Arrange (aca preparamos un pedido pues con un metodo invalido como efectivo)
+    const pedido = {
+      items: ['cuaderno'],
+      total: 50000,
+      metodoPago: 'efectivo'
+    };
+
+
+    const resultado = validarPedido(pedido);
+
+    expect(resultado.valido).to.be.false;
+    expect(resultado.errores).to.include('Método de pago no válido');
   });
 
 });
